0fcdd4183833fb6e1405ba7d441fdbcb9b4a2a55
awsbreathpanda <awsbreathpanda@gmail.com>
2020-09-11 14:54:34 +0800

    feature: Add user management, including register, login and logout

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
-from polls.views import DetailView, IndexView, ResultView
+from polls.views import DetailView, IndexView, LoginView, LogoutView, RegisterView, ResultView
 from django.urls.conf import re_path
 
 urlpatterns = [
+    re_path(r'^logout/$', LogoutView.as_view(), name='logout'),
+    re_path(r'^login/$', LoginView.as_view(), name='login'),
+    re_path(r'^register/$', RegisterView.as_view(), name='register'),
     re_path(r'^detail/(?P<id>\d+)$', DetailView.as_view(), name='detail'),
     re_path(r'^result/(?P<id>\d+)$', ResultView.as_view(), name='result'),
     re_path(r'^$', IndexView.as_view(), name='index'),
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from polls.models import Choice, Question
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404
 from django.views.generic.base import View
 
 # Create your views here.
@@ -21,6 +23,11 @@ class IndexView(View):
 # polls/detail/(id)/
 class DetailView(View):
     def get(self, request, id):
+        if not request.user.is_authenticated:
+            next_url = reverse(
+                'polls:login') + '?next=' + request.get_full_path()
+            return redirect(next_url)
+
         try:
             question = Question.objects.get(id=id)
         except Question.DoesNotExist:
@@ -61,3 +68,64 @@ class ResultView(View):
                       context={
                           'question': question,
                       })
+
+
+# polls/register/
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'polls/register.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not all([username, password]):
+            return redirect(reverse('polls:register'))
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user is not None:
+            return redirect(reverse('polls:register'))
+
+        user = User.objects.create_user(username=username, password=password)
+        user.is_active = 1
+        user.save()
+
+        return redirect(reverse('polls:index'))
+
+
+# polls/login/
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'polls/login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not all([username, password]):
+            return redirect('polls:login')
+
+        print(username + ',' + password)
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return redirect('polls:login')
+
+        if not user.is_active:
+            return redirect('polls:login')
+
+        login(request, user)
+
+        next_url = request.GET.get('next', reverse('polls:index'))
+
+        return redirect(next_url)
+
+
+# polls/logout
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('polls:index'))
